a59dde4174ab053bba60747cf2e7b732db83d34a
GuikiPT <foxzillapt17@gmail.com>
2025-06-26 13:40:51 +0100

    refactor(models): remove unused User and Guild models from the database layer

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,12 +1,8 @@
 import { Sequelize } from 'sequelize';
-import { initUser, User } from './User';
-import { initGuild, Guild } from './Guild';
 import { initYouTubeSubscription, YouTubeSubscription } from './YouTubeSubscription';
 
 export function initModels(sequelize: Sequelize) {
-  initUser(sequelize);
-  initGuild(sequelize);
   initYouTubeSubscription(sequelize);
 }
 
-export { User, Guild, YouTubeSubscription };
+export { YouTubeSubscription };
